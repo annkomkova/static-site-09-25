02d311c947858788311aa7778ff49b811ff011b4
Anna Komkova <ann.komkova98@gmail.com>
2026-05-28 15:49:28 +0300

    partials, sitemap, аналитика и кастомный домен

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,27 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const htmlPages = require('./webpack.pages.js')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const paths = [
+  '/static-site-09-25/index.html',
+  '/static-site-09-25/styleguide.html',
+  '/static-site-09-25/search.html',
+  '/static-site-09-25/pages/articles.html',
+  '/static-site-09-25/pages/dictionary.html',
+  '/static-site-09-25/pages/tests.html',
+  '/static-site-09-25/pages/theory.html',
+  '/static-site-09-25/pages/tests/test1.html',
+  '/static-site-09-25/pages/articles/article1.html',
+  '/static-site-09-25/pages/articles/aloe.html',
+  '/static-site-09-25/pages/articles/cactus.html',
+  '/static-site-09-25/pages/articles/monstera.html',
+  '/static-site-09-25/pages/articles/orchidea.html',
+  '/static-site-09-25/pages/articles/sansevieria.html',
+  '/static-site-09-25/pages/reactBasics.html'
+]
 
 module.exports = {
   entry: {
@@ -57,5 +78,36 @@ module.exports = {
       }
     ]
   },
-  plugins: [...htmlPages, new MiniCssExtractPlugin()]
+  plugins: [
+    ...htmlPages,
+    new MiniCssExtractPlugin(),
+
+    new SitemapPlugin({
+      base: 'https://annkomkova.github.io',
+      paths
+    }),
+
+    new HtmlWebpackPartialsPlugin({
+      path: path.join(__dirname, '../src/partials/footer.html'),
+      location: 'footerPartial',
+      template_filename: '*',
+      priority: 'replace'
+    }),
+
+    new HtmlWebpackPartialsPlugin({
+      path: path.join(__dirname, '../src/partials/analytics.html'),
+      location: 'analytics',
+      template_filename: '*',
+      priority: 'replace'
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/share/CNAME'),
+          to: path.resolve(__dirname, '../docs')
+        }
+      ]
+    })
+  ]
 }
